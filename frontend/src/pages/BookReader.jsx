c062934c06a6f6f8d971bb05ba56aa46d03873e2
Jashvitha Lakshmi <omkaramlakshmi@Jashvithas-MacBook-Pro.local>
2025-12-04 19:19:38 +0530

    final commit

--- frontend/src/pages/BookReader.jsx
+++ frontend/src/pages/BookReader.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CommentsSection from "../components/CommentsSection";
 import { saveBook, deleteBook, markAsRead } from "../api/books";
-import "./bookReader.css";
+import "./BookReader.css";
 
 export default function BookReader() {
   const { id } = useParams();
